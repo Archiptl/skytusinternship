3d2ddb60226177ed64994b7e3a81b5466ecf757e
archi <patelarchi.2505@gmail.com>
2026-08-24 14:24:28 +0530

    setup and basic

---
+++ basic.py
@@ -0,0 +1,58 @@
+#1.print name,age and city
+name=input("Enter your name :")
+print("hello",name)
+
+
+
+#2.two num and their sum
+num1=float(input("Enter Num1 :"))
+num2=float(input("Enter Num2 :"))
+sum=num1+num2
+print(sum)
+
+
+
+#3.tem Celsius (°C) to Fahrenheit (°F)
+celsius =float(input("Enter celsius :"))
+fahrenheit=(celsius * 9/5) + 32
+print(fahrenheit)
+
+
+
+#4.uppercase
+name=input("Enter your name :")
+print(name.upper())
+
+
+
+
+#5.birth year and cal current age
+year =int(input("Enter your birth year:"))
+CA=2026-year
+print("Current age : ",CA)
+
+
+
+#6.swap the values
+a=int(input("Enter variable a :"))
+b=int(input("Enter variable b :"))
+print(a)
+print(b)
+a,b=b,a
+print(a)
+print(b)
+
+
+
+
+#7.area of rectangle 
+length=int(input("Enter the length of rectangle :"))
+width=int(input("Enter the width of rectangle :"))
+area=length*width
+print(area)
+
+
+
+
+#8.check positive or negative num
+num1=float(input("Enter the num1 :"))
